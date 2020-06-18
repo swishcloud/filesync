8a0a8dd6ba2309bbedfbf85140f3ef729e67cbce
swishcloud <flwwd@outlook.com>
2020-06-18 21:13:40 +0800

    static

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./.dist/app
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./.dist/app -a -ldflags '-extldflags "-static"' .
 docker build --tag $IMAGE_TAG -f docker/dockerfile ./.dist
